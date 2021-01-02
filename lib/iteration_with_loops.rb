5eddcdb9240bf2dfaeedd292b0a276ec627d3334
Robert Obi <robobi97@googlemail.com>
2021-01-02 03:07:33 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,16 +1,6 @@
 def join_nested_strings(src)
   
-new_array = []
-row_index = 0 
-while row_index < src.count do 
-  element_index = 0 
-  while element_index < src[row_index].count do 
-    if src[row_index][element_index].class == String
-      new_array << src[row_index][element_index]
-    element_index +=1 
-  end
-  row_index +=1
-end 
+
 
   
   # src will be an Array of Arrays of Strings and Integers
@@ -18,7 +8,18 @@ end
 end
 
 
+words = []
+src.each do |items|
+  items.each do |item|
+    if item.is_a?(String)
+      words << item
+    end
+  end
+end
+words.join(" ")
 
+end 
+  
 
 
 
